48b7c7b4ba25151b0fa0f9963bee3aadb7a2df76
Rob Marquez <robienallen@yahoo.com>
2018-09-29 10:18:02 -0700

    Navigation Drawer created with all modules

--- app/src/main/java/edu/csulb/rob/anacodiam/LoginActivity.java
+++ app/src/main/java/edu/csulb/rob/anacodiam/LoginActivity.java
@@ -3,6 +3,7 @@ package edu.csulb.rob.anacodiam;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -28,6 +29,7 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,7 +48,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     /**
      * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
+     * TODO: remove after connecting to a real authentication system.1
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
@@ -187,6 +189,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+
+            Intent homepageIntent = new Intent(this, HomepageActivity.class);
+            startActivity(homepageIntent);
         }
     }
 
@@ -308,6 +313,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         protected Boolean doInBackground(Void... params) {
             // TODO: attempt authentication against a network service.
 
+
+
             try {
                 // Simulate network access.
                 Thread.sleep(2000);
@@ -315,6 +322,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 return false;
             }
 
+            // DELETE THIS WITH OUR OWN LOGIC
+            // This is where we check on our database if the user's information matches.
+            // If it does, log them in, if not, don't.
             for (String credential : DUMMY_CREDENTIALS) {
                 String[] pieces = credential.split(":");
                 if (pieces[0].equals(mEmail)) {
@@ -323,8 +333,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 }
             }
 
+
             // TODO: register the new account here.
             return true;
+
         }
 
         @Override
